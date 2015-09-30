601ac0eb3cb9df2be929650cb4b7903e35907191
Vladislav <Streamline28@mail.ru>
2015-09-30 20:57:20 +0300

    Library created

---
+++ DSA/Algorithms/AlgorithmSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Search.Algorithms
+{
+    class AlgorithmSort
+    {
+        public static void RandomShuffle(int[] a)
+        {
+            Random random = new Random();
+            for (int i = 1; i < a.Length; i++)
+            {
+                int pos = random.Next(i + 1);
+                int tmp = a[pos];
+                a[pos] = a[i];
+                a[i] = tmp;
+            }
+        }
+    }
+}
